4a66a3b6cb720e7fff280365c2cc4262cda2545a
Eka Bagus Priambudi <ekabagus107@gmail.com>
2025-10-29 22:48:25 +0700

    update responsive

--- src/components/Testimoni/Testimonial.jsx
+++ src/components/Testimoni/Testimonial.jsx
@@ -57,25 +57,25 @@ export default function Testimonial() {
         <h2 className="text-3xl md:text-6xl font-bold text-gray-800">
           Apa <span className="text-orange-500">Kata Partner Kami?</span>
         </h2>
-        <p className="text-gray-600 mt-2">
+        <p className="text-gray-600 text-base mt-2">
           Kolaborasi bareng Lucka bukan cuma soal project, tapi perjalanan <br />
           bersama yang bikin brand makin berkembang
         </p>
       </div>
 
       {/* ===== Grid 2 kolom ===== */}
-      <div className="max-w-4xl md:max-w-4xl mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-8 px-6 place-items-center relative z-10">
+      <div className="max-w-4xl md:max-w-4xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 px-6 place-items-center relative z-10">
         {testimonial.map((item, index) => (
           <div
             key={index}
             className="
               bg-white rounded-2xl shadow-md border border-gray-100 p-6 
               flex flex-col justify-between hover:shadow-lg transition-shadow
-              h-56 w-56 sm:h-64 sm:w-64 md:h-96 md:w-96 lg:h-96 lg:w-96
+              h-80 w-80 sm:h-64 sm:w-64 md:h-96 md:w-96 lg:h-96 lg:w-96
             "
           >
             <Quote className="text-orange-500 w-10 h-10 md:w-14 md:h-14 mb-3" />
-            <p className="text-gray-700 text-xs md:text-xl flex-1">
+            <p className="text-gray-700 text-base md:text-xl flex-1">
               {item.text}
             </p>
             <p className="font-semibold text-gray-900 text-sm mt-3">
